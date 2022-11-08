ddfd65413aca81afaafaf74d29e7c12c1cf9108d
Web Developer <useraccount@WebDeveloperMBP.local>
2022-11-08 12:54:49 -0600

    V1 Launch

--- react/StoreBanner.tsx
+++ react/StoreBanner.tsx
@@ -1,24 +1,249 @@
 import React, { useEffect, useRef, useState } from "react";
-//@ts-expect-error
-import { createPortal } from "react-dom";
+// import { createPortal } from "react-dom";
 import { canUseDOM } from "vtex.render-runtime";
 
 import styles from "./styles.css";
 
 interface StoreBannerProps {
-
+  override: boolean
+  banners: Array<BannerObject>
 }
 
-const StoreBanner: StorefrontFunctionComponent<StoreBannerProps> = ({ }) => {
-  const openGate = useRef(true);
+interface BannerObject {
+  __editorItemTitle: string
+  active: boolean
+  storePath?: string
+  imgSrc: string
+  text: string
+  subText: string
+  link: string,
+  blockClass: string
+}
 
-  return <>Store Banner</>;
+interface ImageBanner {
+  imgSrc: string
+  link: string
+}
+
+interface TextBanner {
+  text: string
+  subText: string
+  link: string
+  blockClass: string
+}
+
+const blankBanner: BannerObject = {
+  __editorItemTitle: "",
+  active: false,
+  imgSrc: "",
+  text: "",
+  subText: "",
+  link: "",
+  blockClass: ""
+}
+
+const StoreBanner: StorefrontFunctionComponent<StoreBannerProps> = ({ override, banners }) => {
+  const openGate = useRef(true);
+  const bannerType = useRef("");
+  const [bannerInfo, setBannerInfo] = useState<any>(blankBanner);
+
+  // Run and reset state.
+  useEffect(() => {
+    if (!openGate.current) return;
+    openGate.current = false;
+
+    bannerType.current = "";
+    setBannerInfo(blankBanner);
+    // If override is set to true, ignore all other banners.
+    override ? runOverRide() : findBanner();
+  })
+
+  // Finds override position in the banners array and calls build banner.
+  const runOverRide = () => {
+    let overRideIndex = -1;
+    for (let index = 0; index < banners.length; index++) {
+      if (banners[index].__editorItemTitle.toLowerCase() === "override") {
+        overRideIndex = index;
+        break;
+      }
+    }
+
+    // If no override banner is found, run findBanner() instead.
+    overRideIndex > -1 ? buildBanner(overRideIndex) : findBanner();
+  }
+
+  // Loops through banners array and finds match with the user's URL.
+  const findBanner = () => {
+    if (!canUseDOM) return;
+    let bannerFound = false;
+
+    const fullPath = window.location.href;
+    const isAdmin = fullPath.includes("siteEditor=true");
+    // Site editor runs in an iFrame. This determines where to find the URL string to match.
+    const storePath = isAdmin ? fullPath.split(".com/store/")[1].split("?")[0].toLowerCase() : fullPath.split(".com/store/")[1].toLowerCase();
+
+    for (let index = 0; index < banners.length; index++) {
+      const bannerItemStore = banners[index].storePath!.toLowerCase();
+
+      const overrideCatch = banners[index].__editorItemTitle.toLowerCase() !== "override";
+      const defaultCatch = banners[index].__editorItemTitle.toLowerCase() !== "default";
+
+      // Don't search Override or Default Banners.
+      if (overrideCatch && defaultCatch) {
+
+        // includes() instead of === to match any reference links.
+        if (storePath.includes(bannerItemStore)) {
+          buildBanner(index);
+          bannerFound = true;
+          break;
+        }
+      }
+    }
+
+    // If no match was found, run show default banner.
+    if (!bannerFound) buildDefaultBanner();
+  }
+
+  // Determines type of banner, Image or Text. Calls appropriate function.
+  // If banner is active, determine type of banner to build.
+  // If banner is inactive, build default banner.
+  const buildBanner = (index: number) => {
+    const banner = banners[index];
+    banner.active ? banner.imgSrc ? buildImagebanner(banner) : buildTextBanner(banner) : buildDefaultBanner();
+  }
+
+  // Builds and renders an Image type banner.
+  const buildImagebanner = (banner: BannerObject) => {
+    const tempBannerInfo: ImageBanner = {
+      imgSrc: banner.imgSrc,
+      link: banner.link || ""
+    }
+    bannerType.current = "image";
+    setBannerInfo(tempBannerInfo);
+  }
+
+  // Builds and renders a Text type banner.
+  const buildTextBanner = (banner: BannerObject) => {
+    const tempTextBanner: TextBanner = {
+      text: banner.text,
+      subText: banner.subText || "",
+      link: banner.link || "",
+      blockClass: banner.blockClass || ""
+    }
+    bannerType.current = "text";
+    setBannerInfo(tempTextBanner);
+  }
+
+  // No override and no individual store banner is found for current URL.
+  // Finds default banner position in banners array and builds appropriate
+  // type of banner.
+  const buildDefaultBanner = () => {
+    let defaultIndex = -1;
+
+    for (let index = 0; index < banners.length; index++) {
+      if (banners[index].__editorItemTitle.toLowerCase() === "default") {
+        defaultIndex = index;
+        break;
+      }
+    }
+
+    // This condition should only fire if there's no default banner set. Will show blank banner.
+    if (defaultIndex === -1) {
+      bannerType.current = "";
+      return;
+    }
+
+    const banner = banners[defaultIndex];
+    banner.imgSrc ? buildImagebanner(banner) : buildTextBanner(banner);
+  }
+
+  // This component lessens layout shift on first paint.
+  const Blank = () => <div className={styles.blankBanner} />
+
+  const ImageBannerComponent = () =>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <img src={bannerInfo.imgSrc} className={styles.image} />
+      </div>
+    </div>
+
+  const TextBannerComponent = () =>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.text}>{bannerInfo.text}</div>
+        {bannerInfo.subText && <div className={bannerInfo.blockClass ? `${styles.subText}--${bannerInfo.blockClass}` : styles.subText}>
+          {bannerInfo.subText}
+        </div>}
+      </div>
+    </div>
+
+  const LinkedBannerComponent = () => <a href={bannerInfo.link} className={styles.link}><BannerComponent /></a>
+
+  const BannerComponent = () => bannerType.current === "image" ? <ImageBannerComponent /> : bannerType.current === "text" ? <TextBannerComponent /> : <Blank />;
+
+  return bannerInfo.link ? <LinkedBannerComponent /> : <BannerComponent />
 }
 
 StoreBanner.schema = {
   title: "Store Banner",
   type: "object",
-  properties: {}
+  properties: {
+    override: {
+      title: "Override Switch",
+      description: "Turn on to display the Override banner on all store pages.",
+      type: "boolean",
+      default: false
+    },
+    banners: {
+      title: "Banners",
+      type: "array",
+      items: {
+        properties: {
+          __editorItemTitle: {
+            title: "Name",
+            type: "string"
+          },
+          active: {
+            title: "Activate",
+            type: "boolean",
+            default: true
+          },
+          storePath: {
+            title: "Store Path",
+            description: "Text after the .com/store/ in URL.",
+            type: "string"
+          },
+          imgSrc: {
+            title: "Image Source URL",
+            description: "Optional | Absolute Path to image. Image will override text.",
+            type: "string"
+          },
+          text: {
+            title: "Banner Text",
+            description: "Optional | Leave blank if Image is being used.",
+            type: "string",
+          },
+          subText: {
+            title: "Banner SubText or Button Text",
+            description: "Optional | Leave blank if not desired.",
+            type: "string"
+          },
+          blockClass: {
+            title: "Style",
+            description: "Optional | Absolute or Relative path to URL.",
+            type: "string",
+            enum: ["standard", "button"],
+            default: "standard"
+          },
+          link: {
+            title: "Link",
+            description: "Optional | Absolute or Relative path to URL.",
+            type: "string"
+          }
+        }
+      }
+    }
+  }
 }
 
 export default StoreBanner;
